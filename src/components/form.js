f164f382bea42ac67697ff25836574f9a72aa513
CodeXRook <lacekitfly@gmail.com>
2019-04-11 04:30:14 -0400

    update div

--- src/components/form.js
+++ src/components/form.js
@@ -1,23 +1,26 @@
 import Form from './components/form';
 
-class Form extends React.Component{
-        constructor(props){
-            super(props);
+class Form extends React.Component {
+    constructor(props) {
+        super(props);
 
-            this.state ={
-                name: '',
-                price: '',
-                image: []    
+        this.state = {
+            name: '',
+            price: '',
+            image: []
+        }
+    }
+
+    render() {
+        return (
+            <form className="needs-validation" novalidate>
+                <div className="col-md-4 mb-3"></div>
+                <label for="validationCustom01"> Enter Product </label>
+                <input type="text" className="form-control" id="validationCustom01" placeholder=" " value="" required />
+                </div>
+
+                
+                )
             }
-        }
-
-        render(){
-            return(
-                <form className="needs-validation" novalidate>
-                     <div className="col-md-4 mb-3"></div>
-                     <label for="validationCustom01"> Enter Product </label>
-                     <input type="text" className="form-control" id="validationCustom01" placeholder=" " value="" required />
-            )
-        }
-}
+    }
 export default Form:
