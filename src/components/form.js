9d9fd65dae6add99e532ca7571fa73b8d6381df7
CodeXRook <lacekitfly@gmail.com>
2019-04-11 04:23:07 -0400

    updated state

--- src/components/form.js
+++ src/components/form.js
@@ -1,2 +1,10 @@
 import Form from './components/form';
 
+class Form extends React.Component{
+        constructor(props){
+            super(props);
+
+            this.state ={
+                name:''            }
+        }
+}
