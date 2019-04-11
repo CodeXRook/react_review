fbeac61c5e9df9e89fc84a7146beb6e596360478
CodeXRook <lacekitfly@gmail.com>
2019-04-11 04:05:34 -0400

    updated new price amount

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
         {name:'Lv tie', image:'https://us.louisvuitton.com/images/is/image/lv/1/PP_VP_AS/louis-vuitton--M71869_PM2_Front%20view.jpg?wid=382&hei=100', price: '$550'},
         {name:'Lv Belt', image:'https://us.louisvuitton.com/images/is/image/lv/1/PP_VP_AS/louis-vuitton--LP0109_PM2_Front%20view.jpg?wid=382&hei=100', price: '$185'},
         {name:'Lv ORAGE FOR MEN', image:'https://us.louisvuitton.com/images/is/image/lv/1/PP_VP_AS/louis-vuitton--LP0051_PM2_Front%20view.jpg?wid=382&hei=100', price: '$250'},
-        {name:'Lv Robe', image:'https://us.louisvuitton.com/images/is/image/lv/1/PP_VP_AS/louis-vuitton--FGCO37AYZ571_PM2_Front%20view.jpg?wid=382&hei=100', price: '$460'},
+        {name:'Lv Robe', image:'https://us.louisvuitton.com/images/is/image/lv/1/PP_VP_AS/louis-vuitton--FGCO37AYZ571_PM2_Front%20view.jpg?wid=382&hei=100', price: '$460.00'},
         {name:'Lv Women shoes', image:'https://us.louisvuitton.com/images/is/image/lv/1/PP_VP_AS/louis-vuitton--AG5U7BMI61_PM2_Front%20view.jpg?wid=189&hei=100', price: '$1,090.00'}
         ]
        }
