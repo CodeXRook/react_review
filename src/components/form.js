fe299d10f2b6ae7cc57330639e34d5a417261fbb
CodeXRook <lacekitfly@gmail.com>
2019-04-11 22:47:13 -0400

    checking form

--- src/components/form.js
+++ src/components/form.js
@@ -1,47 +1,13 @@
 import React from 'react';
 
-const Form = (props) = {
-        return (
-
-        )   
-
-}
-
-        this.state = {
-            input: '',
-            name: 'Lv Belt',
-            price: '$1000.00',
-            image: ['https://us.louisvuitton.com/images/is/image/lv/1/PP_VP_AS/louis-vuitton--M6065S_PM2_Front%20view.jpg?wid=382&hei=100']
-        }
-    }
-   
-  handleInputChange = (e) => {
-    this.setState ({ input: e.target.value});
-
-  }
-
-
-
-    handleSubmit = (e) =>{
-const { input, name, price, image} = this.state;
-
-    }
-
-
-    render() {
-
-        const { name} = this.state;
-        const {image} = this.state;
-        const { price} = this.state;
-
+const Form = (props) => {
         return (
             <>
                 <form className="form">
 
                     <label> Product Name </label>
                      <input type="text" onChange={this.handleInputChange} value={this.state.value.input}/>
-     
-                    
+         
 
                      <label> Show Image</label>
                     <input type="text" onChange={this.handleInputChange} value={this.state.value.input} /> 
@@ -52,7 +18,7 @@ const { input, name, price, image} = this.state;
                  <button onClick={this.handleSubmit}> Submit form </button>
                 </form>
             </>
-        )
-    }
-}
+            )   
+ }
+        
 export default Form;
