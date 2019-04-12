c90f501f3eb95bff6ddde2a516f390f5a50f1180
CodeXRook <lacekitfly@gmail.com>
2019-04-11 23:04:58 -0400

    redid state and Handle events

--- src/components/form.js
+++ src/components/form.js
@@ -1,24 +1,53 @@
 import React from 'react';
 
-const Form = (props) => {
+class Form extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            input: '',
+            value: '',
+            name: 'Lv Belt',
+            price:  '$1000.00',
+            image: ['https://us.louisvuitton.com/images/is/image/lv/1/PP_VP_AS/louis-vuitton--M6065S_PM2_Front%20view.jpg?wid=382&hei=100']
+        
+        }
+    }
+handleInputChange =(e)=>{
+const input = e.target.value
+this.setState ({input});
+
+}
+
+handleClick = (e) => {
+    console.log('this is:', this);
+}
+
+    render() {
         return (
             <>
-                <form className="form">
+            <form className="needs-validation" novalidate>
+                <div className="col-md-4 mb-3">
+                <label for="validationCustom01"> Enter Product </label>
+                <input type="text" className="form-control" id="validationCustom01" placeholder=" " value="" required />
+                </div>
 
-                    <label> Product Name </label>
-                     <input type="text" onChange={this.handleInputChange} value={this.state.value.input}/>
-         
+                <div className="col-md-4 mb-3">
+                <label for="validationCustom02"> Show Image</label>
+                <input type="text" className="form-control" id="validationCustom02" placeholder="" value="" required />
+                </div>
 
-                     <label> Show Image</label>
-                    <input type="text" onChange={this.handleInputChange} value={this.state.value.input} /> 
+                   <div className="col-md-4 mb-3">
+                   <label for="validationCustom03"> Price </label>
+                   <input type="text" class="form-control" id="validationCustom03" placeholder=" " value="" required />
+                   </div>
 
-                    <label > Price </label>
-                    <input type="text" placeholder=" " value={this.state.value.input} required />
-
-                 <button onClick={this.handleSubmit}> Submit form </button>
-                </form>
-            </>
-            )   
- }
-        
+                    <div className="form-group">
+                    <button className="btn btn-primary" type="submit" onClick= {this.handleClick} >Submit form</button>
+                    </div>
+                    </form>
+</>
+                )
+            }
+    }
 export default Form;
