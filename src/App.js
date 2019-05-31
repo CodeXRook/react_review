91f9b618f06236a6a25f060cc557c26738cb9ee5
CodeXRook <lacekitfly@gmail.com>
2019-05-30 21:49:42 -0400

    updated vs setting fonts

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import fire from './config/Fire';
  
 import './App.css';
 
@@ -12,36 +11,32 @@ class App extends Component {
     }
   }
 
-//COMPONENT DID MOUNT
-componentDidMount(){
-  this.authListener();
-}
+// //COMPONENT DID MOUNT
+// componentDidMount(){
+//   this.authListener();
+// }
 
-  authListener() {
-    firebase.auth().onAuthStateChanged((user) => {
-      // console.log(user);
-      if(user){
-        this.setState({user});
-        // localStorage.setItem('user', user.uid);
-      }else{
-        this.setState({ user: null });
-        // localStorage.removeItem('user');
-      }
-    });
-  }
+//   authListener() {
+//     firebase.auth().onAuthStateChanged((user) => {
+//       // console.log(user);
+//       if(user){
+//         this.setState({user});
+//         // localStorage.setItem('user', user.uid);
+//       }else{
+//         this.setState({ user: null });
+//         // localStorage.removeItem('user');
+//       }
+//     });
+//   }
 
 
   render() {
     return(
-
+<div className> Hello world </div>
       //Checking is user is logged in 
       //If this.state.user is true? Then great! we render home, push him to home. other wise log in, trya again
-      <div className="App">
-        
-        {this.state.user ? (< Home />) : (<Login/>)} 
-
-      </div>
-    );
+     
+    )
   }
 }
 
