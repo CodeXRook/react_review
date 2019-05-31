f77cba3b36588e8e9920fb42bf0940674c268cae
CodeXRook <lacekitfly@gmail.com>
2019-05-30 21:08:32 -0400

    firebase sign in

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,9 @@
-const firebaseConfig = {
+import app from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/storage';
+
+
+const config = {
     apiKey: "AIzaSyAQTeKt6k_rwqrAko0SrhbBvGgpyIdGYiY",
     authDomain: "usertest-2b112.firebaseapp.com",
     databaseURL: "https://usertest-2b112.firebaseio.com",
@@ -7,3 +12,7 @@ const firebaseConfig = {
     messagingSenderId: "197584488025",
     appId: "1:197584488025:web:b608b0350881237d"
   };
+
+  app.initializeApp(config);
+
+export default app;
